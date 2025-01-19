eaeee4e9f8c8569b5ba2bf913410a2cb082eeb15
jps1990 <jeanphilippesaumier@gmail.com>
2025-01-18 21:32:27 -0500

    update app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import { useEffect } from 'react';
 import { Ghost, Plus } from 'lucide-react';
 import { useStore } from './store';
 import ChatRoom from './components/ChatRoom';
